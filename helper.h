6d724a6498a494b4a6cda5a95d5860e1c1125c39
mislavzanic <mislavzanic3@gmail.com>
2020-12-10 10:40:19 +0100

    Started timing, finished multiplication

--- helper.h
+++ helper.h
@@ -2,8 +2,21 @@
 #define MATRIX_CHAIN_MULTIPLICATION_HELPER_H
 
 #include <stdlib.h>
+#include <assert.h>
+#include <time.h>
 
 typedef int** mat;
+
+double time_matr_multipl(mat (*func)(), void (*Free)(), int start, int end, mat* args1, int* args2, int** args3)
+{
+    clock_t time1, time2;
+    time1 = clock();
+        mat A = func(start, end, args1, args2, args3);
+    time2 = clock();
+    Free(A, args2[start - 1]);
+
+    return ((double)(end - start)) / CLOCKS_PER_SEC;
+}
 
 mat create_mat(int n, int m)
 {
@@ -32,6 +45,44 @@ mat multiply(mat A, mat B, int n, int k, int m)
     }
 
     return C;
+}
+
+void Free(mat A, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        free(A[i]);
+    }
+
+    free(A);
+}
+
+mat multiply_array(int start, int end, mat* mat_array, int* dim_array, int** S)
+{
+    if (start == end)
+    {
+        return mat_array[start];
+    }
+    else
+    {
+        mat A, B, C;
+        A = multiply_array(start, S[start][end], mat_array, dim_array, S);
+        B = multiply_array(S[start][end] + 1, end, mat_array, dim_array, S);
+        C = multiply(A, B, dim_array[start - 1], dim_array[S[start][end]], dim_array[end]);
+        Free(A, dim_array[start - 1]);
+        Free(B, dim_array[S[start][end]]);
+        return C;
+    }
+}
+
+void time_matrix_multiplications(int** P, int** S, int N)
+{
+    //mat* mat_array = generate_mat_array();
+    mat result;
+    double result1, result2;
+    result1 = time_matr_multipl(multiply_array, Free, 1, 6, mat_array, dim_array, S);
+    result2 = time_matr_multipl(multiply_array, Free, 1, 6, mat_array, dim_array, S);
+    printf("%f %f", result1, result2);
 }
 
 #endif //MATRIX_CHAIN_MULTIPLICATION_HELPER_H
--- main.c
+++ main.c
@@ -7,10 +7,7 @@
 
 typedef int** mat;
 
-int P[N][N];
-int S[N][N];
-
-int opt_solution(int *dim_array, int n)
+int opt_solution(int *dim_array, int n, int** P, int** S)
 {
     for (int l = 2; l < n; ++l)
     {
@@ -31,26 +28,13 @@ int opt_solution(int *dim_array, int n)
     return P[1][n - 1];
 }
 
-void recreate(int start, int end, int* index, int i)
+void init(int** P, int** S)
 {
-    if (start == end)
-    {
-        printf("A_%d", start);
-        index[start] = i;
-    }
-    else
-    {
-        printf("(");
-        recreate(start, S[start][end], index, i + 1);
-        recreate(S[start][end] + 1, end, index, i + 1);
-        printf(")");
-    }
-}
-
-void init()
-{
+    P = (int**)malloc(N * sizeof(int*)); S = (int**)malloc(N * sizeof(int*));
     for (int i = 0; i < N; ++i)
     {
+        P[i] = (int*)malloc(N * sizeof(int));
+        S[i] = (int*)malloc(N * sizeof(int));
         for (int j = 0; j < N; ++j)
         {
             if (i == j)
@@ -66,14 +50,13 @@ void init()
 int main(int argc, char** argv)
 {
 //    if (argc < 2) printf("Call: ./program_name infile_path");
-
     char* filepath = argv[1];
     int dim_array[] = {30, 35, 15, 5, 10, 20, 25};
     int* index = (int*)malloc(6 * sizeof(int));
-    init();
-    int opt_dim = opt_solution(dim_array, 7);
-    recreate(1, 6, index, 0);
-    printf("\n");
+    int **P, **S; init(P, S);
+
+    int opt_dim = opt_solution(dim_array, 7, P, S);
     printf("Optimalan broj mnozenja je %d\n", opt_dim);
+    time_matrix_multiplications(P, S, N);
     return 0;
 }
