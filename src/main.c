0f9cbffb065bfc9c60265d4fea8d90a454079de0
mislavzanic <mislavzanic3@gmail.com>
2021-01-09 15:30:53 +0100

    Bugfixes

--- src/main.c
+++ src/main.c
@@ -27,6 +27,18 @@ long opt_solution(const ll *dim_array, int n, ll** P, ll** S)
     return P[1][n - 1];
 }
 
+void print_opt_solution(ll** S, int start, int end)
+{
+    if (start == end) printf("A_%d", start);
+    else
+    {
+        printf("(");
+        print_opt_solution(S, start, S[start][end]);
+        print_opt_solution(S, S[start][end] + 1, end);
+        printf(")");
+    }
+}
+
 void init(ll*** P, ll*** S)
 {
     *P = (ll**)malloc(N * sizeof(ll*)); *S = (ll**)malloc(N * sizeof(ll*));
@@ -63,8 +75,14 @@ ll* read_from_file(char* filepath, int *n)
 
 int main(int argc, char** argv)
 {
-    if (argc < 2) printf("Call: ./program_name infile_path");
-    char* filepath = argv[1];
+    char* filepath;
+    if (argc < 2)
+    {
+        printf("Call: ./program_name infile_path");
+        printf("File path:\n");
+        scanf("%s", filepath);
+    }
+    filepath = argv[1];
     int arr_len;
     ll sum = 0;
     ll* dim_array = read_from_file(filepath, &arr_len);
@@ -73,9 +91,11 @@ int main(int argc, char** argv)
     ll opt_dim = opt_solution(dim_array, arr_len, DP, Par_placement);
     printf("Optimalan broj mnozenja je %lld\n", opt_dim);
 
-    for (int i = 1; i < arr_len; i++) sum += dim_array[i] * dim_array[i+1];
+    for (int i = 1; i < arr_len - 1; i++) sum += dim_array[i] * dim_array[i+1];
     printf("Broj mnozenja po redu je %lld\n", sum * dim_array[0]);
-
+    printf("Optimalan poredak: ");
+    print_opt_solution(Par_placement, 1, arr_len - 1);
+    printf("\n");
     time_matrix_multiplications(Par_placement, dim_array, arr_len);
     free(dim_array);
     return 0;
